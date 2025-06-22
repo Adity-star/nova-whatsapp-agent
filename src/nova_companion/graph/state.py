25dfd874c7232c30da124e1bb1948fe73d2cf789
Aditya Akuskar <Adiiii@Aditya.Akuskar>
2025-06-22 20:48:02 +0530

    working/ host required

--- src/nova_companion/graph/state.py
+++ src/nova_companion/graph/state.py
@@ -1,16 +1,16 @@
-from typing import Optional
+from typing import List, Optional
 
-from langgraph.graph import MessagesState
-from pydantic import Field
+from langchain_core.messages import AnyMessage
+from pydantic import BaseModel, Field
 
 
-class AICompanionState(MessagesState):
+class AICompanionState(BaseModel):
     """State class for the AI Companion workflow.
 
     Extends MessagesState to track conversation history and maintains the last message received.
 
     Attributes:
-        last_message (AnyMessage): The most recent message in the conversation, can be any valid
+        last_message (AnyMessage):ha The most recent message in the conversation, can be any valid
             LangChain message type (HumanMessage, AIMessage, etc.)
         workflow (str): The current workflow the AI Companion is in. Can be "conversation", "image", or "audio".
         audio_buffer (bytes): The audio buffer to be used for speech-to-text conversion.
@@ -18,6 +18,7 @@ class AICompanionState(MessagesState):
         memory_context (str): The context of the memories to be injected into the character card.
     """
 
+    messages: List[AnyMessage] = Field(default_factory=list)
     summary: str = Field(default="")
     workflow: str = Field(default="conversation")
     audio_buffer: Optional[bytes] = Field(default=None)
